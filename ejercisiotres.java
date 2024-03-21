260c61dc3d4c5607ef9723a3206b292364fa60c3
imaginedrag0ns <125479085+imaginedrag0ns@users.noreply.github.com>
2024-03-20 19:45:43 -0500

    Add files via upload
    taller semana 7

---
+++ ejercisiotres.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author diego
+ */
+public class ejercisiotres {
+   public static double convertidor(double dolares) {
+        double factorConversion = 1.33250;
+        double euros = dolares * factorConversion;
+        return euros;
+    }
+    public static void main(String[] args) {
+        // Casos de prueba
+        double dolares1 = 200;
+        double euros1 = convertidor(dolares1);
+        System.out.println(dolares1 + " USD = " + euros1 + " EUR");
+
+        double dolares2 = 600;
+        double euros2 = convertidor(dolares2);
+        System.out.println("\n" + dolares2 + " USD = " + euros2 + " EUR");
+    }
+}
